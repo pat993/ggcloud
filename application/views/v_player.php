5d87236df14cfefc7e1648426b81eb9666e31c56
Patra Abdala <patra.abdala@gmail.com>
2024-05-14 08:26:28 +0000

    commit

--- application/views/v_player.php
+++ application/views/v_player.php
@@ -46,9 +46,9 @@
             box-sizing: border-box;
         }
 
-        body {
+        html, body {
             width: 100%;
-            height: 100vh;
+            height: 100%;
             margin: 0;
             /* display: flex; */
             justify-content: center;
