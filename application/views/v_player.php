7e59fb78e35ec2e84738ab87efa40c6031dad5f3
Patra Abdala <patra.abdala@gmail.com>
2024-07-25 04:25:36 +0000

    Minox Fix

--- application/views/v_player.php
+++ application/views/v_player.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
@@ -15,21 +14,19 @@
     <link rel="icon" type="image/x-icon" href="/ggc/images/ggc_play.png">
     <link rel="stylesheet" href="/ggc/styles/custom.css">
 </head>
-
 <body style="background-color: black; position: relative" class="theme-dark" data-highlight="highlight-red" data-gradient="body-default">
     <div class="battery">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-    </div>
+        <div class="straight"></div>
+        <div class="curve"></div>
+        <div class="center"></div>
+        <div class="inner"></div>
+      </div>
 
     <div class="DraggableDiv">
-        <button class="btn btn-dark rounded-xl" id="fullscreen-toggle"><i class="fas fa-expand" style="font-size: 9px;"></i></button>
-        <button class="btn btn-dark rounded-xl mt-1" id="slide-toggle"><i class="fas fa-ellipsis-h" style="font-size: 9px;"></i></button>
+        <button class="btn btn-dark rounded-xl" id="fullscreen-toggle"><i class="fas fa-expand" style="font-size: 10px;"></i></button>
+        <button class="btn btn-dark rounded-xl mt-1" id="slide-toggle"><i class="fas fa-ellipsis-h" style="font-size: 10px;"></i></button>
     </div>
-
+    
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="/ggc/bundle.js"></script>
     <script src="/ggc/scripts/bootstrap.min.js"></script>
@@ -37,28 +34,5 @@
     <script src="/ggc/draggable.js"></script>
     <script src="/ggc/scripts/ifvisible.js"></script>
     <script src="/ggc/scripts/main.js"></script>
-
-    <script>
-        function stream_quality() {
-            var e = document.getElementById("stream_quality").value;
-            if (e == "1") {
-                document.getElementById("in_bitrate").value = "1524288";
-                document.getElementById("in_fps").value = "40";
-                document.getElementById("in_max_w").value = "1080";
-                document.getElementById("in_max_h").value = "1080";
-            } else if (e == "2") {
-                document.getElementById("in_bitrate").value = "2524288";
-                document.getElementById("in_fps").value = "40";
-                document.getElementById("in_max_w").value = "1080";
-                document.getElementById("in_max_h").value = "1080";
-            } else if (e == "3") {
-                document.getElementById("in_bitrate").value = "5524288";
-                document.getElementById("in_fps").value = "40";
-                document.getElementById("in_max_w").value = "1080";
-                document.getElementById("in_max_h").value = "1080";
-            }
-        }
-    </script>
 </body>
-
 </html>
--- ggc/scripts/main.js
+++ ggc/scripts/main.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Control panel toggle
-        $(document).ready(function() {
-            setTimeout(function() {
-                $(".control-wrapper").animate({
-                    height: "toggle"
-                });
-            }, 1000);
-            $("#slide-toggle").click(function() {
-                $(".control-wrapper").animate({
-                    height: "toggle"
-                });
-                // Check if the setting-box is currently visible
-                if ($('.more-box').is(':visible')) {
-                    $('#input_show_more').trigger('click');
-                }
+    $(document).ready(function() {
+        setTimeout(function() {
+            $(".control-wrapper").animate({
+                height: "toggle"
             });
-            $("#fullscreen-toggle").click(function() {
-                document.body.requestFullscreen();
+        }, 1000);
+        
+        $("#slide-toggle").click(function() {
+            $(".control-wrapper").animate({
+                height: "toggle"
             });
+            
+            // Check if the setting-box is currently visible
+            if ($('.more-box').is(':visible')) {
+                $('#input_show_more').trigger('click');
+            }
         });
-    
+
+        $("#fullscreen-toggle").click(function() {
+            document.body.requestFullscreen();
+        });
+    });
+
     slideToggle.addEventListener('click', function() {
         controlWrapper.style.display = controlWrapper.style.display === 'none' ? 'block' : 'none';
         const moreBox = document.querySelector('.more-box');
@@ -39,33 +42,53 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
-
-    // Mobile browser detection
-    function isMobileBrowser() {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    }
-
-    if (isMobileBrowser()) {
-        $('.DraggableDiv').draggableTouch();
-    }
-
-    // Stream quality control
-    function setStream(br) {
-        document.getElementById("in_bitrate").value = br;
-        setTimeout(function() {
-            const changeVideoBtn = document.getElementById("btn_change_video");
-            if (changeVideoBtn) {
-                changeVideoBtn.click();
-            }
-        }, 1000);
-    }
-
-    // Visibility change handlers
-    ifvisible.on("blur", function() {
-        setStream("524288");
-    });
-
-    ifvisible.on("wakeup", function() {
-        setStream("2524288");
-    });
 });
+
+// Mobile browser detection
+function isMobileBrowser() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobileBrowser()) {
+    $('.DraggableDiv').draggableTouch();
+}
+
+// Stream quality control
+function setStream(br) {
+    document.getElementById("in_bitrate").value = br;
+    setTimeout(function() {
+        const changeVideoBtn = document.getElementById("btn_change_video");
+        if (changeVideoBtn) {
+            changeVideoBtn.click();
+        }
+    }, 1000);
+}
+
+// Visibility change handlers
+ifvisible.on("blur", function() {
+    setStream("524288");
+});
+
+ifvisible.on("wakeup", function() {
+    setStream("2524288");
+});
+
+function stream_quality() {
+    var e = document.getElementById("stream_quality").value;
+    if (e == "1") {
+        document.getElementById("in_bitrate").value = "1524288";
+        document.getElementById("in_fps").value = "40";
+        document.getElementById("in_max_w").value = "1080";
+        document.getElementById("in_max_h").value = "1080";
+    } else if (e == "2") {
+        document.getElementById("in_bitrate").value = "2524288";
+        document.getElementById("in_fps").value = "40";
+        document.getElementById("in_max_w").value = "1080";
+        document.getElementById("in_max_h").value = "1080";
+    } else if (e == "3") {
+        document.getElementById("in_bitrate").value = "5524288";
+        document.getElementById("in_fps").value = "40";
+        document.getElementById("in_max_w").value = "1080";
+        document.getElementById("in_max_h").value = "1080";
+    }
+}
